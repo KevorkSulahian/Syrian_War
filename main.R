1f03e8ca2766199bbad29d47839260215577c624
herooooooooo <kevolig1234@gmail.com>
2018-08-06 18:36:30 +0400

    adding stuff yo

--- main.R
+++ main.R
@@ -1,11 +1,15 @@
 library(dplyr)
 library(ggplot2)
 library(lubridate)
+library(stringr)
+
+
 syria <- read.csv("data.csv")
 
 
-syria <- data[c("data_id","event_date", "year", "event_type", "actor1", "assoc_actor_1", "actor2" ,"assoc_actor_2",
+syria <- syria[c("data_id","event_date", "year", "event_type", "actor1", "assoc_actor_1", "actor2" ,"assoc_actor_2",
                 "admin1", "admin2", "admin3", "geo_precision", "fatalities")]
+
 
 
 syria <- syria[-1,]
@@ -23,17 +27,51 @@ syria$fatalities <- as.numeric(syria$fatalities)
 colnames(syria) <- c("ID", "date", "year", "type", "team1", "team1_assister", "team2",
                      "team2_assister", "state", "city", "street", "geo_precision", "fatalities")
 
+fighters <- as.data.frame(unique(syria$team1))
+
+military_syria <- str_extract_all(syria$team1, pattern = "^Military Forces of Syria")
+
+syria_attack <- {}
+j = 1
+for(i in military_syria) {
+  if(length(i)> 0) {
+    syria_attack[j] = T
+    j = j+1
+  } else {
+    syria_attack[j] = F
+    j = j+1
+  }
+}
+syria$government_attack <- syria_attack
 by_month <- syria %>%
   group_by(month = floor_date(date, "month")) %>%
-  summarize(death = sum(fatalities)) 
+  summarize(death = sum(fatalities),
+            attack = sum(government_attack)) 
 
 
 by_month$year <- substr(by_month$month, 0,04)
 by_month$month_abb <- months(as.Date(by_month$month), abbreviate = TRUE)
 by_month$month <- as.numeric(format(as.Date(by_month$month), "%m"))
-
+by_month$attack <- as.numeric(by_month$attack)
 
 ggplot(by_month, aes(x = factor(month, levels = c(1:12)), y = death, group = year, colour=year)) + 
   geom_line() +
   geom_point() +
-  scale_x_discrete(breaks = by_month$month, labels = by_month$month_abb, name = "months")
+  scale_x_discrete(breaks = by_month$month, labels = by_month$month_abb) +
+  labs(x = "months", y = "deaths", title = "difference in the number of deaths between 2017 and 2018")
+
+ggplot(by_month, aes(x = factor(month, levels = c(1:12)), y = attack, group = year, colour=year)) + 
+  geom_line() +
+  geom_point() +
+  scale_x_discrete(breaks = by_month$month, labels = by_month$month_abb) +
+  labs(x = "months", y = "attack", title = "difference in the number of attack between 2017 and 2018")
+
+### by state
+
+by_state <- syria %>%
+  group_by(state) %>%
+  summarize(death = sum(fatalities),
+            attack = sum(government_attack)) 
+
+
+ggplot(by_state, aes(y  =  death, x = state)) + geom_bar(stat = "identity")
